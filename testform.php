6c20e61a64acc130d0b50bbfaeaf0c24da38cd4b
Brens <brenlysquintero@gmail.com>
2016-11-30 16:14:01 -0400

    form

---
+++ testform.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD>
+<TITLE> Recepcion de solicitudes </TITLE>
+<meta http-equiv="content-type" content="text/html ; charset="iso-8859-1" ">
+</HEAD>
+<BODY>
+<h1> Rellena los campos(form.html)</h1>
+<form action="analisis.php" method="post">
+Nombre del solicitante
+<input type="tex" name="name">
+<input type="submit">
+</form>
+</BODY>
+</HTML>
