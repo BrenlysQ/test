02103329dc0d36fb67ac8ea48f672867d684fa40
Brens <brenlysquintero@gmail.com>
2016-11-30 20:40:36 -0400

    modified

--- testform.php
+++ testform.php
@@ -6,8 +6,21 @@
 <BODY>
 <h1> Rellena los campos(form.html)</h1>
 <form action="analisis.php" method="post">
-Nombre del solicitante
+Nombre del solicitante:
 <input type="tex" name="name">
+Dependencia:
+<input type="tex" name="dependencia">
+Tipo de Solicitud:
+<SELECT NAME=“Tipo de solicitud">
+<OPTION VALUE= “Sistema" SELECTED>Sistema
+<OPTION VALUE=“Soporte">Soporte
+<OPTION VALUE=“Servicio">Servicio
+<OPTION VALUE=“Evento">Evento
+</SELECT>
+<?PHP
+$solicitud = $_REQUEST[‘solicitud’];
+print ($solicitud);
+?>
 <input type="submit">
 </form>
 </BODY>
