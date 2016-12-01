9361b59ae271b95daaf8faaec8519db9e8fc98c2
Brens <brenlysquintero@gmail.com>
2016-11-30 21:30:37 -0400

    probando desde localhost

--- testform.php
+++ testform.php
@@ -4,23 +4,19 @@
 <meta http-equiv="content-type" content="text/html ; charset="iso-8859-1" ">
 </HEAD>
 <BODY>
-<h1> Rellena los campos(form.html)</h1>
+<h1> Recepcion de solicitudes</h1>
 <form action="analisis.php" method="post">
 Nombre del solicitante:
 <input type="tex" name="name">
 Dependencia:
 <input type="tex" name="dependencia">
 Tipo de Solicitud:
-<SELECT NAME=“Tipo de solicitud">
+<SELECT NAME="Tipo de solicitud">
 <OPTION VALUE= “Sistema" SELECTED>Sistema
 <OPTION VALUE=“Soporte">Soporte
 <OPTION VALUE=“Servicio">Servicio
 <OPTION VALUE=“Evento">Evento
 </SELECT>
-<?PHP
-$solicitud = $_REQUEST[‘solicitud’];
-print ($solicitud);
-?>
 <input type="submit">
 </form>
 </BODY>
