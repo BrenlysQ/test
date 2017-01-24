0529965140f6397b259850de4184b0f39c098626
BrenlysQ <brenlysquintero@gmail.com>
2017-01-23 21:16:42 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     function conexion(){
-        $conexion = mysqli_connect('localhost','root','','BD');
-        mysqli_set_charset($conexion, "utf8");
+        $conexion = mysqli_connect('localhost','root','','Solicitudes');
+        mysqli_set_charset($conexion,"utf8");
         return $conexion;
     }
 ?>
