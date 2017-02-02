736a52f48a7a8307b8f5fada1b59e9b7733fc22b
BrenlysQ <brenlysquintero@gmail.com>
2017-02-02 09:13:10 -0400

    Add files via upload

--- procesarsolicitud.php
+++ procesarsolicitud.php
@@ -1,30 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>HI</title>
-</head>
-<body>
-	<?php 
-	if($_POST['Solicitud']=="Desarrollo")
-	{
-		header("location: Desarrollo.php" );
-	}
- 	
- 	if($_POST['Solicitud']=="Soporte")
-	{
-		header("location: Soporte.php" );
-	}
+<?php 
+		include('conexion.php');
+		$conexion = conexion();
 
-	if($_POST['Solicitud']=="Servicio")
-	{
-		header("location: Servicio.php" );
-	}
 
-	if($_POST['Solicitud']=="Evento")
-	{
-		header("location: Evento.php" );
-	}
+		$solicitante = $_POST['name'];
+
+		$dependencia = $_POST['dependencia'];
+
+		$tipoSolicitud = $_POST['Solicitud'];
+
+
+		$sql = "INSERT INTO solicitante VALUES ('$solicitante','$dependencia', '$tipoSolicitud')";
+
+		$resultado = mysqli_query($conexion,$sql) or die(mysqli_error($conexion));
+
+		if($_POST['Solicitud']=="Desarrollo")
+		{
+			header("location: Desarrollo.php" );
+		}
+	 	
+	 	if($_POST['Solicitud']=="Soporte")
+		{
+			header("location: Soporte.php" );
+		}
+
+		if($_POST['Solicitud']=="Servicio")
+		{
+			header("location: Servicio.php" );
+		}
+
+		if($_POST['Solicitud']=="Evento")
+		{
+			header("location: Evento.php" );
+		}
+
+		mysqli_close($conexion);
 
  	?>
-</body>
-</html>
